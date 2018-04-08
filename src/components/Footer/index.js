1d1a7b30a31f22a695c2028e0772717a6a6d9fc0
Jonathan Westman <jwestman@activelamp.com>
2018-04-07 21:48:39 -0600

    Updated footer link and 404 container

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './footer.scss'
 const Footer = () => (
   <div className="footer">
     <div className="copyright">
-      <p>© 2018 Jonathan Westman | View the source on <a href="">GitHub</a></p>
+      <p>© 2018 Jonathan Westman | View the source on <a href="https://github.com/jonathanwestman/jonathanwestman.github.io">GitHub</a></p>
     </div>
   </div>
 )
